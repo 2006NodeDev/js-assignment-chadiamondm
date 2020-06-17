060b020dd937d5cad59bab691deaec518e44119b
chadiamondm <chadiamondm@gmail.com>
2020-06-16 22:34:28 -0400

    completed palindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,23 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+  //change all letters to lower case
+  let someStrLower = someStr.toLowerCase();
+
+  let split_pal = someStrLower.split("");
+
+  let rev_pal = split_pal.reverse();
+
+  let join_pal = rev_pal.join("");
+
+  if (someStrLower === join_pal) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(isPalindrome("can"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("eye"));
+console.log(isPalindrome("Hawaii"));
+console.log(isPalindrome("madam"));
